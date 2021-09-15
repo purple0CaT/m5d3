5cb8da65c1bec9ce2aea65ba53283ef6bb070ced
Istvan <steven.ford.j@gmail.com>
2021-09-15 17:51:20 +0300

    fix

--- src/components/blog/blog-list/index.jsx
+++ src/components/blog/blog-list/index.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Row, Col } from "react-bootstrap";
 import BlogItem from "../blog-item";
-import posts from "../../../data/posts.json";
 
 export default class BlogList extends Component {
   state = {
@@ -16,7 +15,6 @@ export default class BlogList extends Component {
       let response = await fetch(url);
       let data = await response.json();
       if (response.ok) {
-        console.log(data);
         this.setState({ blog: data });
       } else {
         console.log("Some error");
@@ -30,7 +28,7 @@ export default class BlogList extends Component {
       <Row>
         {this.state.blog && this.state.blog.map((post) => (
           <Col md={4} style={{ marginBottom: 50 }}>
-            <BlogItem key={post.title} {...post} />
+            <BlogItem key={post.title + post.cover} {...post} />
           </Col>
         ))}
       </Row>
